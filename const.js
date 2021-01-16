029a46834b73a75b377798abdb392afd2274c880
yuxianbo <xianbo.yxb@alibaba-inc.com>
2021-01-16 09:51:15 +0800

    init

---
+++ const.js
@@ -0,0 +1,99 @@
+// 礼物列表
+const giftList = [
+  {
+    // 皮卡丘
+    img: 'https://img.alicdn.com/imgextra/i2/O1CN01hq7rEN1L2QA4xPDJZ_!!6000000001241-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2001
+  },
+  {
+    // 两个男人
+    img: 'https://img.alicdn.com/imgextra/i3/O1CN01n4uuUl1raNEDVxzUu_!!6000000005647-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2002
+  },
+  {
+    // jerry
+    img: 'https://img.alicdn.com/imgextra/i1/O1CN01G8QqsK1URSM2ceJLS_!!6000000002514-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2003
+  },
+  {
+    // 招财猫
+    img: 'https://img.alicdn.com/imgextra/i4/O1CN01DckIfx1sQ7z2L19P2_!!6000000005760-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2004
+  },
+  {
+    // mickey
+    img: 'https://img.alicdn.com/imgextra/i2/O1CN01G3RneO1JGxVAzZ8Xi_!!6000000001002-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2005
+  },
+  {
+    // iron man
+    img: 'https://img.alicdn.com/imgextra/i2/O1CN01EpqnL71m7uXFqCTM5_!!6000000004908-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2006
+  },
+  {
+    // 爱心
+    img: 'https://img.alicdn.com/imgextra/i2/O1CN017xxXlm1jHa3Cbx9IB_!!6000000004523-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2007
+  },
+  {
+    // bape
+    img: 'https://img.alicdn.com/imgextra/i3/O1CN01XnXnw21EehxX3xqbz_!!6000000000377-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2008
+  },
+  {
+    // tom
+    img: 'https://img.alicdn.com/imgextra/i1/O1CN01F2JZXT1qxRYymFMWJ_!!6000000005562-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2009
+  },
+  {
+    // 原宿风
+    img: 'https://img.alicdn.com/imgextra/i1/O1CN01QuuSXp1geX3FZj3Fc_!!6000000004167-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2010
+  },
+  {
+    // 哆啦A梦
+    img: 'https://img.alicdn.com/tfs/TB16uwarPMZ7e4jSZFOXXX7epXa-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2011
+  },
+  {
+    // 小黄人
+    img: 'https://img.alicdn.com/imgextra/i4/O1CN01EQmfG826g4O8FcwkH_!!6000000007690-2-tps-317-550.png',
+    isDrawed: false,
+    gift: 'IPHONE手机',
+    due: '2021-10-31',
+    value: 2012
+  },
+];
